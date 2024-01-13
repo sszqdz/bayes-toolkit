76502abb1963672d2ef7e0f3fdc470e4dffac304
Moran <boboohappy@163.com>
2024-01-14 01:07:56 +0800

    [opt] optimize environment

--- environment/env.go
+++ environment/env.go
@@ -5,8 +5,8 @@
 package environment
 
 import (
+	"fmt"
 	"os"
-	"path/filepath"
 	"sync"
 
 	"github.com/sszqdz/bayes-toolkit/dirr"
@@ -19,14 +19,14 @@ type ENV string
 const (
 	EnvDebug   ENV = "debug"
 	EnvRelease ENV = "release"
-
-	key      string = "ENV"
-	fileName string = ".env"
 )
 
 var (
-	Deepth  = 3
-	loadEnv = sync.OnceValue[string](extractEnv)
+	ENVKey   = "ENV"
+	FileName = ".env"
+	Deepth   = 3
+	loadEnv  = sync.OnceValue[string](extractEnv)
+	loadFile = sync.OnceValue[*viper.Viper](loadFileConf)
 )
 
 // do not auto inject, use when runtime
@@ -42,26 +42,45 @@ func (e ENV) String() string {
 	return string(e)
 }
 
+func Load(key string) string {
+	return extractKey(key)
+}
+
 func extractEnv() string {
+	return extractKey(ENVKey)
+}
+
+func extractKey(key string) string {
 	// Extract environment variable
 	env := os.Getenv(key)
 	if len(env) > 0 {
 		return env
 	}
+	// Extract file variable
+	vFile := loadFile()
+	if vFile != nil {
+		return vFile.GetString(key)
+	}
+
+	return ""
+}
+
+func loadFileConf() *viper.Viper {
 	// Upward recursive search for .env files
-	curPath, err := os.Executable()
+	curDir, err := os.Getwd()
 	panicErr(err)
-	curDir := filepath.Dir(curPath)
-	envFilePath, err := dirr.FindFileInParentDirs(fileName, curDir, Deepth)
+	fmt.Println("curDir: " + curDir)
+	envFilePath, err := dirr.FindFileInParentDirs(FileName, curDir, Deepth)
+	fmt.Println("envFilePath: " + envFilePath)
 	if os.IsNotExist(err) {
-		return ""
+		return nil
 	}
 	panicErr(err)
 
-	vEnv := viper.New()
-	vEnv.SetConfigFile(envFilePath)
-	panicErr(vEnv.ReadInConfig())
-	return vEnv.GetString(key)
+	vFile := viper.New()
+	vFile.SetConfigFile(envFilePath)
+	panicErr(vFile.ReadInConfig())
+	return vFile
 }
 
 func panicErr(err error) {
