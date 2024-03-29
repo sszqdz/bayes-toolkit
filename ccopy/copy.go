9f50f0e79bb6a0c38d10ca2a13c51d70d9fca68e
Moran <boboohappy@163.com>
2024-03-29 16:59:59 +0800

    [fix] fix deep copy return

--- ccopy/copy.go
+++ ccopy/copy.go
@@ -2,6 +2,6 @@ package ccopy
 
 import "github.com/jinzhu/copier"
 
-func DeepCopy(toValue interface{}, fromValue interface{}) {
-	_ = copier.CopyWithOption(toValue, fromValue, copier.Option{DeepCopy: true})
+func DeepCopy(toValue interface{}, fromValue interface{}) error {
+	return copier.CopyWithOption(toValue, fromValue, copier.Option{DeepCopy: true})
 }
